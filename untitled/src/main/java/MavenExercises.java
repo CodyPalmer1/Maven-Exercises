3797a013279a649c6c4883c8c466f42daa857bbb
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-19 16:47:37 -0500

    Updates to MavenExercises

--- untitled/src/main/java/MavenExercises.java
+++ untitled/src/main/java/MavenExercises.java
@@ -1,8 +1,26 @@
 import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
 
 public class MavenExercises {
     public static void main(String[] args) {
-        String test = StringUtils.upperCase("lowercase");
-        System.out.println(test);
+        Scanner sc = new Scanner(System.in);
+//        String test = StringUtils.upperCase("lowercase");
+//        System.out.println(test);
+
+        System.out.print("Enter something: ");
+        String userInput = sc.nextLine();
+
+        System.out.println("You entered: " + userInput);
+        if(!StringUtils.isNumeric(userInput)){
+            System.out.println(userInput + " is not a number");
+        } else {
+            System.out.println(userInput + " is a number");
+        }
+
+        String swap = StringUtils.swapCase(userInput);
+        System.out.println("Flipped Case: " + swap);
+        String reverse = StringUtils.reverse(userInput);
+        System.out.println("Reversed: " + reverse);
+
     }
 }
