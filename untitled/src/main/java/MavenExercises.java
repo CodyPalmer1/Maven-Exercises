85a49213da9986be4fb41bd3490f18684cd582c9
Cody Palmer <codyryanpalmer@gmail.com>
2022-10-19 16:10:42 -0500

    Updates to maven-exercises

---
+++ untitled/src/main/java/MavenExercises.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenExercises {
+    public static void main(String[] args) {
+        String test = StringUtils.upperCase("lowercase");
+        System.out.println(test);
+    }
+}
